4714421433243c38ff4491e55d2a8be3eb997ece
Mbasa-N <mbasanjomane@gmail.com>
2024-03-13 14:19:05 +0200

    Initial Commit

---
+++ Scripts/dataset_generator.R
@@ -0,0 +1,96 @@
+if (!require("tidyverse")) install.packages("tidyverse", dependencies = TRUE)
+library(openxlsx)
+library(tidyverse)
+library(dplyr)
+library(redcapAPI)
+library(RMySQL)
+library(summarytools)
+library(readxl)
+library(haven)
+library(xlsx)
+library(survival)
+library(conflicted)
+
+
+source("Scripts/functions.R")
+
+print("getting REDCap connection")
+rcon <- getREDCapConnection(2)
+path <- "./Data/"
+output_file <- paste0('dataset',format(Sys.time(), '%d_%B_%Y'),'.xlsx')
+
+events <- exportEvents(rcon)
+
+events <- as.list(events$unique_event_name)
+
+instruments <- exportMappings(rcon)
+
+today <- as.POSIXct(Sys.time())
+
+
+for(event in events){
+  forms <- subset(instruments, instruments$unique_event_name==event)
+  forms <- as.vector(forms$form)
+  
+  print(paste0("Event: ", event))
+  
+  if(event != "index_enrolment_arm_1"){
+    forms <- append(forms,"index_screening_and_consent",0)
+    
+    temp <- getREDCapRecords(event, forms, NULL, TRUE)
+  }
+  
+  if(event != "index_hhc_investig_arm_1" & event!= "household_level_da_arm_1"){
+    temp <- getREDCapRecords(event, forms, NULL, TRUE)
+    
+    if(event!="index_enrolment_arm_1"){
+      temp <- temp[-c(5:100)]
+    }
+    
+    temp$record_id <- as.numeric(temp$record_id)
+    assign(paste('raw_data', event, sep = '_'), temp)
+  }
+  
+  if(event=="index_hhc_investig_arm_1"){
+    temp <- getREDCapRecords(event, forms, NULL, TRUE)
+    
+    if(event!="index_enrolment_arm_1"){
+      temp <- temp[-(5:100)]
+    }
+    
+    temp$record_id <- as.numeric(temp$record_id)
+    assign(paste('raw_data', event, sep = '_'), temp)
+  }
+  
+  if(event=="household_level_da_arm_1"){
+    temp <- getREDCapRecords(event, forms, NULL, TRUE)
+    
+    if(event!="index_enrolment_arm_1"){
+      temp <- temp[-(5:100)]
+    }
+    
+    temp$record_id <- as.numeric(temp$record_id)
+    assign(paste('raw_data', event, sep = '_'), temp)
+  }
+}
+
+
+write.table(raw_data_index_enrolment_arm_1, 'Data/Baseline.csv', sep = ",", row.names = FALSE)
+
+write.table(raw_data_index_hhc_investig_arm_1, 'Data/HHCI.csv', sep = ",", row.names = FALSE)
+
+write.table(raw_data_household_level_da_arm_1, 'Data/HH level Data.csv', sep = ",", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
